245a97987c4ce6e0a4c46da6185b996116d254a0
AbdulrahmanMagdey <abdomagdey5@gmail.com>
2025-04-04 13:19:15 +0200

    For Ipv4, Follow the naming convention and use the final format for the tests

--- Main.kt
+++ Main.kt
@@ -1,19 +1,3 @@
 fun isValidIp(ip: String): Boolean {
-    if (!ip.contains(".")) return false
-
-    val segments = ip.split(".")
-
-    if (segments.size != 4) return false
-
-    for (segment in segments) {
-        if (segment.isEmpty() || !segment.all { it.isDigit() }) return false
-
-        val num = segment.toIntOrNull() ?: return false
-
-        if (num !in 0..255) return false
-
-        if ( segment.length > 1 && segment.startsWith("0") ) return false
-    }
-
-    return true
+   return false
 }
--- TestCases.kt
+++ TestCases.kt
@@ -6,151 +6,116 @@ fun check(name: String, result: Boolean, correct: Boolean, input: String) {
     }
 }
 
-fun returnTrueWhenIpIsValid(ip: String) {
+fun main() {
+    var ip = "192.168.1.1"
     check(
-        name = "Return True When Ip Is Valid",
+        name = "Should Return True When Ip Is Valid",
         result = isValidIp(ip),
         correct = true,
         input = ip
     )
-}
 
-fun returnFalseWhenIpIsInvalid(ip: String) {
+    ip = "256.100.50.25"
     check(
-        name = "Return False When Ip Isn't Valid",
+        name = "Should Return False When Ip has big segment",
         result = isValidIp(ip),
         correct = false,
         input = ip
     )
-}
 
-fun returnFalseWhenIpHasBigSegment() {
-    val ip = "256.100.50.25"
+    ip = "192.168.01.1"
     check(
-        name = "Return False When Ip has big segment",
+        name = "Should Return False When Ip has leading zero",
         result = isValidIp(ip),
         correct = false,
         input = ip
     )
-}
 
-fun returnFalseWhenIpHasLeadingZero() {
-    val ip = "192.168.01.1"
+    ip = "192.168.1"
     check(
-        name = "Return False When Ip has leading zero",
+        name = "Should Return False When Ip has invalid segment count",
         result = isValidIp(ip),
         correct = false,
         input = ip
     )
-}
 
-fun returnFalseWhenIpHasInvalidSegmentCount() {
-    val ip = "192.168.1"
+    ip = "192.168.1"
     check(
-        name = "Return False When Ip has invalid segment count",
+        name = "Should Return False When Ip has invalid segment count",
         result = isValidIp(ip),
         correct = false,
         input = ip
     )
-}
 
-fun returnFalseWhenIpHasMoreThanFourSegments() {
-    val ip = "192.168.1.1.1"
+    ip = "192.168.1.1.1"
     check(
-        name = "Return False When Ip has more than four segments",
+        name = "Should Return False When Ip has more than four segments",
         result = isValidIp(ip),
         correct = false,
         input = ip
     )
-}
 
-fun returnFalseWhenIpHasCommasInsteadOfDots() {
-    val ip = "192,168,1,1"
+    ip = "192,168,1,1"
     check(
-        name = "Return False When Ip has commas instead of dots",
+        name = "Should Return False When Ip has commas instead of dots",
         result = isValidIp(ip),
         correct = false,
         input = ip
     )
-}
 
-fun returnFalseWhenIpHasDoubleDots() {
-    val ip = "1..1"
+    ip = "1..1"
     check(
-        name = "Return False When Ip has double dots",
+        name = "Should Return False When Ip has double dots",
         result = isValidIp(ip),
         correct = false,
         input = ip
     )
-}
 
-fun returnFalseWhenIpHasLeadingDot() {
-    val ip = ".1.1.1.1"
+    ip = ".1.1.1.1"
     check(
-        name = "Return False When Ip has leading dot",
+        name = "Should Return False When Ip has leading dot",
         result = isValidIp(ip),
         correct = false,
         input = ip
     )
-}
-
-fun returnFalseWhenIpHasAlphabets() {
-    val ip = "192.1x1.1.1"
+    ip = "192.1x1.1.1"
     check(
-        name = "Return False When Ip has alphabets",
+        name = "Should Return False When Ip has alphabets",
         result = isValidIp(ip),
         correct = false,
         input = ip
     )
-}
 
-fun returnFalseWhenIpHasNegativeNumbers() {
-    val ip = "-1.2.3.4"
+    ip = "-1.2.3.4"
     check(
-        name = "Return False When Ip has negative numbers",
+        name = "Should Return False When Ip has negative numbers",
         result = isValidIp(ip),
         correct = false,
         input = ip
     )
-}
 
-fun returnFalseWhenIpHasSpaceBetweenSegments() {
-    val ip = "192. 168.1.1"
+    ip = "192. 168.1.1"
     check(
-        name = "Return False When Ip has space between segments",
+        name = "Should Return False When Ip has space between segments",
         result = isValidIp(ip),
         correct = false,
         input = ip
     )
-}
 
-fun returnFalseWhenIpHasZeroPrefix() {
-    val ip = "00.0.0.0"
+    ip = "00.0.0.0"
     check(
-        name = "Return False When Ip has zero prefix",
+        name = "Should Return False When Ip has zero prefix",
         result = isValidIp(ip),
         correct = false,
         input = ip
     )
-}
 
-fun main() {
-    returnFalseWhenIpHasBigSegment()
-    returnFalseWhenIpHasLeadingZero()
-    returnFalseWhenIpHasInvalidSegmentCount()
-    returnFalseWhenIpHasMoreThanFourSegments()
-    returnFalseWhenIpHasCommasInsteadOfDots()
-    returnFalseWhenIpHasDoubleDots()
-    returnFalseWhenIpHasLeadingDot()
-    returnFalseWhenIpHasAlphabets()
-    returnFalseWhenIpHasNegativeNumbers()
-    returnFalseWhenIpHasSpaceBetweenSegments()
-    returnFalseWhenIpHasZeroPrefix()
+    ip = "1000. 01.255.ab.1"
+    check(
+        name = "Should Return False When Ip Isn't Valid",
+        result = isValidIp(ip),
+        correct = false,
+        input = ip
+    )
 
-    returnTrueWhenIpIsValid("192.168.1.1")
-    returnTrueWhenIpIsValid("8.8.8.8")
-    returnTrueWhenIpIsValid("1.2.3.4")
-    returnTrueWhenIpIsValid("172.16.254.1")
-
-    returnFalseWhenIpIsInvalid("1000.16.254.10")
 }
